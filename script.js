a02542312331f84e36962a5db6354d1ee65d04bb
Mahendras1ngh <iammahendrasingh@hotmail.com>
2020-09-15 09:29:35 +0530

    format of the game

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#btn");
+const para = document.querySelector("#para");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+
+btn.addEventListener("click", roll);
+
+function roll() {}
